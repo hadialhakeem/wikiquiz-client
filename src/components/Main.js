5f41fdd69d41fc69b81cb5ba83963f48a5a4b81b
hadialhakeem <hadihakeem2@gmail.com>
2021-01-16 00:06:59 -0500

    Title Works now

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react"
 
 class Main extends React.Component {
     constructor() {
@@ -13,8 +13,8 @@ class Main extends React.Component {
     render(){
 
         return (
-            <div textAlign="center">
-                <h1>WikiMe</h1>
+            <div>
+                <Text fontSize="6xl" align={'center'}>WikiMe</Text>
             </div>
         )
     }
