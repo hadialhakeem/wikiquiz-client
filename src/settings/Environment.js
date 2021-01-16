13a82235a6baba4657bccbe0f982cfd9412d4f2f
hadialhakeem <hadihakeem2@gmail.com>
2021-01-15 23:38:01 -0500

    added backend api's

--- src/settings/Environment.js
+++ src/settings/Environment.js
@@ -1,14 +1,14 @@
 export const EnvironmentDev = {
     name: 'dev',
     production: false,
-    apiUrl: 'http://127.0.0.1:5000/api',
+    apiUrl: 'http://127.0.0.1:5000',
 };
 
 
 export const EnvironmentProd = {
     name: 'prod',
     production: true,
-    apiUrl: 'http://127.0.0.1:5000/api',
+    apiUrl: 'http://127.0.0.1:5000',
 };
 
 let Environment = {};
