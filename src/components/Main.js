e3a88530a56ed150ef87734d6b3e3cfa6f0ff96a
hadialhakeem <hadihakeem2@gmail.com>
2021-01-15 23:47:34 -0500

    added Main component

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Main extends React.component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            quiz: null
+        }
+    }
+
+    render(){
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
