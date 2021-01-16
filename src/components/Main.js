335def50ccb7598b46f1c15a15158ef481a980f1
hadialhakeem <hadihakeem2@gmail.com>
2021-01-16 18:33:01 -0500

    v1 working

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,7 +27,6 @@ class Main extends React.Component {
                 .then(res => {
                     const { quiz } = res.data;
                     this.setState({quiz});
-
                 })
                 .catch(err => {
                     console.log({err})
