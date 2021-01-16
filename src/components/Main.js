e8a65ca191eff33f44a10ad3e84edc63cd9a8e5a
Yasith Ellewela <yasith.ellewela02@gmail.com>
2021-01-16 01:20:24 -0500

    Added search bar and formatted button location

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text } from "@chakra-ui/react"
 import { Button, ButtonGroup } from "@chakra-ui/react"
 import { Box } from "@chakra-ui/react"
+import { Input } from "@chakra-ui/react"
 import { StarIcon } from '@chakra-ui/icons'
 import { Badge } from "@chakra-ui/react"
 import { Image } from "@chakra-ui/react"
@@ -32,7 +33,8 @@ class Main extends React.Component {
                 <Text fontSize="xl">Welcome! WikiMe allows you to search for a Wikipedia topic to be quizzed on. Try it out below!</Text>
               </div>
               <div>
-                <Button colorScheme="blue">Search</Button>
+                <Input variant="outline" size="lg" w="50%" margin="30px" placeholder="Wikipedia Topic"/>
+                <Button colorScheme="blue" size="lg">Search</Button>
               </div>
             </div>
         )
