424336a3e771685132f29a4b1d30603f3f1a7f60
hadialhakeem <hadihakeem2@gmail.com>
2021-01-16 16:14:21 -0500

    started modifying Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import { Heading } from "@chakra-ui/react"
 import { Button } from '@chakra-ui/react';
+import {ArrowForwardIcon} from "@chakra-ui/icons";
 
 class Quiz extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            currentQuestion: 0
+            currentQuestion: 0,
+            score: 0,
+            isSelected: false
         }
     }
 
-    render () {
+    onChoose = () => {
+        console.log("SELECTED")
+        this.setState({isSelected: true});
+    }
+
+    onNext = () => {
         const { currentQuestion } = this.state;
+        let newQuestion = currentQuestion + 1;
+        this.setState({isSelected: false, currentQuestion: newQuestion})
+    }
+
+    render () {
+        const { currentQuestion, isSelected } = this.state;
         let { questions } = this.props;
 
-        let renderedQuestion = <Question qDict={questions[currentQuestion]} />
+        let renderedQuestion = <Question qDict={questions[currentQuestion]} onChoose={this.onChoose}/>
+
+        let nextQuestionButton = (
+            <div>
+                <Button rightIcon={<ArrowForwardIcon />} colorScheme="teal" variant="outline" onClick={()=>{console.log("HI")}}>
+                    Next Question
+                </Button>
+            </div>
+        )
 
         return (
             <div>
                 {renderedQuestion}
+                <br />
+                {isSelected && !isSelected &&
+                    {nextQuestionButton}
+                }
             </div>
         )
     }
@@ -35,7 +61,7 @@ class Quiz extends React.Component {
 
  */
 function Question(props) {
-    const { qDict } = props;
+    const { qDict, onChoose } = props;
 
     return (
         <div>
@@ -45,57 +71,34 @@ function Question(props) {
                 </Heading>
             </div>
             <div>
-                <Button
-                  size="md"
-                  height="175px"
-                  width="500px"
-                  border="2px"
-                  marginRight="15px"
-                  marginTop="0px"
-
-                  borderColor="green.500"
-                >
-                    {qDict.options[0]}
-                </Button>
-                <Button
-                  size="md"
-                  height="175px"
-                  width="500px"
-                  border="2px"
-                  borderColor="green.500"
-                >
-                    {qDict.options[1]}
-                </Button>
+                {questionOption(qDict.options[0], onChoose, "15px","0px")}
+                {questionOption(qDict.options[1], onChoose, "0px", "0px")}
             </div>
             <div>
-                <Button
-                  size="md"
-                  height="175px"
-                  width="500px"
-                  border="2px"
-                  marginRight="15px"
-                  marginTop="15px"
-                  borderColor="green.500"
-                >
-                    {qDict.options[2]}
-                </Button>
-                <Button
-                  size="md"
-                  height="175px"
-                  width="500px"
-                  border="2px"
-                  marginTop="15px"
-                  borderColor="green.500"
-                >
-                    {qDict.options[3]}
-                </Button>
+                {questionOption(qDict.options[2], onChoose, "15px", "15px")}
+                {questionOption(qDict.options[3], onChoose, "0px", "15px")}
             </div>
-
         </div>
-
     )
 }
 
+const questionOption = (buttonText, onSubmit, marginRight, marginTop) => {
+
+    return (
+        <Button
+            size="md"
+            height="175px"
+            width="500px"
+            border="2px"
+            borderColor="green.500"
+            marginTop={marginTop}
+            marginRight={marginRight}
+            onSubmit={()=>onSubmit()}
+        >
+            {buttonText}
+        </Button>
+    )
+}
 
 
 
