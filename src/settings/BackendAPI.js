1b7316de52394c6c39d1ec8395684eeb6c02db74
hadialhakeem <hadihakeem2@gmail.com>
2021-01-16 18:14:11 -0500

    added stuff

--- src/settings/BackendAPI.js
+++ src/settings/BackendAPI.js
@@ -1,28 +1,25 @@
 import request from 'axios';
 import Environment from "./Environment";
+
 class BackendAPI {
 
     static ApiURL = Environment.apiUrl;
 
     static get = (path='') => {
 
-        const apiCompletionPromise = request({
+        return request({
             method: 'get',
             url: `${BackendAPI.ApiURL}/${path}/`,
         });
-
-        return apiCompletionPromise;
     };
 
     static post = (path, data) => {
 
-        const apiCompletionPromise = request({
+        return request({
             method: 'post',
             data: data,
             url: `${BackendAPI.ApiURL}/${path}`,
-        });
-
-        return apiCompletionPromise
+        })
     }
 
     static generateQuiz = (title) => {
